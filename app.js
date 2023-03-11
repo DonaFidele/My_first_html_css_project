60dd0d97877789b67877db95170a5c8c40c7ef1b
Dona <dona@kali.com>
2023-03-12 00:56:42 +0100

    Implement the html and css code about the website page

---
+++ app.js
@@ -0,0 +1,15 @@
+let toggle = document.querySelector('.toggle');
+let body = document.querySelector("body");
+
+toggle.addEventListener("click",fonction()(body.classList.toggle("open")));
+
+/*const hamburgerButton = document.querySelector(".nav-toggler")
+const navigation = document.querySelector("nav")
+
+hamburgerButton.addEventListener("click",toggleNav)
+
+function toggleNav()
+{
+    hamburgerButton.classList.toggle("active")
+}*/
+
